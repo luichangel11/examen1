6230e80ee298f3ece025a91c1ca04907e17777c9
luis angel <luichtec@gmail.com>
2023-05-30 13:19:45 -0700

    primer commit

---
+++ bin/examen.dart
@@ -0,0 +1,5 @@
+import 'package:examen/examen.dart' as examen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${examen.calculate()}!');
+}
---
+++ test/examen_test.dart
@@ -0,0 +1,8 @@
+import 'package:examen/examen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
